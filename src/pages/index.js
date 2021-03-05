68a49176d0b7558c8fdfa676870d35384dbe48a2
hassaankhan235 <hassaankhan235@gmail.com>
2021-03-05 17:08:07 +0500

    added index.js

---
+++ src/pages/index.js
@@ -0,0 +1,29 @@
+import React, {useContext} from "react"
+import IdentityContext from '../../IdentityContext'
+
+import Layout from './layout/layout'
+import LoginCard from './app/components/LoginCard'
+import WelcomeCard from './app/components/WelcomeCard'
+import AppLogedin from './app/components/appLoggedIn'
+import Styles from './app/components/Dash.module.css'
+
+
+export default function Home(props) {
+
+  const {user} = useContext(IdentityContext)
+
+
+  return (
+    <div className= {`bg-secondary ${Styles.fh}`} >
+    <Layout /> 
+    
+    {user.email ? 
+    <> <WelcomeCard /> 
+   <AppLogedin />
+  </>
+  : 
+    <LoginCard />
+    }
+    </div>
+  )
+}
