ebb926ec3d74e5b7b49a4e65059e5c31fcdb0af6
hassaankhan235 <hassaankhan235@gmail.com>
2021-03-05 16:14:19 +0500

    delete contentful images

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,27 +4,28 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
-module.exports = {
-  /* Your site config here */
-  siteMetadata : {
-    title: 'The New Site'
-  },
-  plugins: [ {
-    resolve: `gatsby-source-filesystem`,
-    options: {
-      name: `images`,
-      path: `${__dirname}/src/images`,
-    },
-  },
-  {
-    resolve: `gatsby-source-contentful`,
-    options: {
-      spaceId: `2ecehfn5iq5j`,
-      // Learn about environment variables: https://gatsby.dev/env-vars
-      accessToken: 'N9ytIjNy1-IDZMD7_L6CJB4o56JhKQV8npkPMgC1ysE',
-    },
-  },
-  `gatsby-transformer-sharp`, 
-  `gatsby-plugin-sharp`
-  ],
-}
+ 
+// module.exports = {
+//   /* Your site config here */
+//   siteMetadata : {
+//     title: 'The New Site'
+//   },
+//   plugins: [ {
+//     resolve: `gatsby-source-filesystem`,
+//     options: {
+//       name: `images`,
+//       path: `${__dirname}/src/images`,
+//     },
+//   },
+//   {
+//     resolve: `gatsby-source-contentful`,
+//     options: {
+//       spaceId: `2ecehfn5iq5j`,
+//       // Learn about environment variables: https://gatsby.dev/env-vars
+//       accessToken: 'N9ytIjNy1-IDZMD7_L6CJB4o56JhKQV8npkPMgC1ysE',
+//     },
+//   },
+//   `gatsby-transformer-sharp`, 
+//   `gatsby-plugin-sharp`
+//   ],
+// }
