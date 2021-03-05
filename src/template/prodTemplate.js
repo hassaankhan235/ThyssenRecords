9d0f7bf4e784343c99c0b4c5a3e0fc2312477ee5
hassaankhan235 <hassaankhan235@gmail.com>
2021-03-05 16:58:22 +0500

    removed contentful errors

--- src/template/prodTemplate.js
+++ src/template/prodTemplate.js
@@ -1,32 +1,32 @@
-import React from "react"
-import { graphql } from "gatsby"
-import Image from 'gatsby-image'
+// import React from "react"
+// import { graphql } from "gatsby"
+// import Image from 'gatsby-image'
 
-const ComponentName = ({ 
-  data: {prod: {
-    title, price, subTitle, image:{
-      fixed
-    }
-  }} }) => 
-  <pre>
-  {subTitle}
-  <Image fixed={fixed} />
-  {title}
-</pre>
+// const ComponentName = ({ 
+//   data: {prod: {
+//     title, price, subTitle, image:{
+//       fixed
+//     }
+//   }} }) => 
+//   <pre>
+//   {subTitle}
+//   <Image fixed={fixed} />
+//   {title}
+// </pre>
 
-export const query = graphql`
-query hkQuery($subTitle:String) {
-    prod: contentfulItems(subTitle:{eq: $subTitle}) {
-      title
-      price
-      subTitle
-      image{
-        fixed{
-          ...GatsbyContentfulFixed
-        }
-      }
-    }
-  }
-`
+// export const query = graphql`
+// query hkQuery($subTitle:String) {
+//     prod: contentfulItems(subTitle:{eq: $subTitle}) {
+//       title
+//       price
+//       subTitle
+//       image{
+//         fixed{
+//           ...GatsbyContentfulFixed
+//         }
+//       }
+//     }
+//   }
+// `
 
-export default ComponentName
+// export default ComponentName
